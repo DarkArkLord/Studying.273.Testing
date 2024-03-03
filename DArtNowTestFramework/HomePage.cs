af1c22f870d23a40b88b04ee54a4fb769076c04b
DarkLord <dfhrhfan12@yandex.ru>
2024-03-03 20:33:53 +0400

    Add Home page

---
+++ DArtNowTestFramework/HomePage.cs
@@ -0,0 +1,26 @@
+﻿using DArtTests;
+using DBaseSiteTestFramework;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DArtNowTestFramework
+{
+    public class HomePage : DBasePage
+    {
+        public HomePage(DarkWebDriver driver) : base(driver) { }
+
+        public void OpenPage()
+        {
+            driver.SetUrl("https://artnow.ru/");
+        }
+
+        public void OpenLeftMenu()
+        {
+            driver.FindByXPath("//*[@id=\"left_container\"]/div/ul[2]/li[15]/div/span").Click();
+        }
+
+        public void OpenElementByText(string element)
+        {
+            driver.FindByXPath($"//*[contains(text(), '{element}')]").Click();
+        }
+    }
+}
--- DArtTests/ArtNowTests.cs
+++ DArtTests/ArtNowTests.cs
@@ -1,45 +1,24 @@
+using DArtNowTestFramework;
+using DBaseSiteTestFramework;
 using NUnit.Framework;
 using NUnit.Framework.Interfaces;
 using System.Threading.Tasks;
 
 namespace DArtTests
 {
-    [TestFixture(2)]
-    [TestFixture(5)]
-    public class ArtNowTests
+    public class ArtNowTests : DBaseSiteTests
     {
-        private readonly WebDriverType driverType;
-
-        public ArtNowTests(WebDriverType driverType)
-        {
-            this.driverType = driverType;
-            driver = null;
-        }
-
-        private DarkWebDriver driver;
-        private int t;
-
-        [SetUp]
-        public void Setup()
-        {
-            t = q;
-            //driver = DarkWebDriver.InitFirefox();
-            //driver.SetUrl("https://artnow.ru/");
-        }
+        public ArtNowTests(WebDriverType driverType) : base(driverType) { }
 
         [Test]
         public void Test1()
         {
-            Assert.Positive(t);
-            //driver.FindByXPath("//*[@id=\"left_container\"]/div/ul[2]/li[15]/div/span").Click();
-            //driver.FindByXPath("//*[@id=\"left_container\"]/div/ul[2]/li[8]/a").Click();
-            //Task.Delay(1000).Wait();
-        }
+            var home = new HomePage(driver);
+            home.OpenPage();
+            home.OpenLeftMenu();
+            home.OpenElementByText("Вышитые картины");
 
-        [TearDown]
-        public void StopBrowser()
-        {
-            //driver.Close();
+            Task.Delay(1000).Wait();
         }
     }
 }
